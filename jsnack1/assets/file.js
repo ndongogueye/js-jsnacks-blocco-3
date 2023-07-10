cd23383cbde25f563d1401bfcdb3e8da2a59d39e
ndongogueye <bayeg2003@gmail.com>
2023-07-10 09:40:37 +0200

    piccola correzione

--- jsnack1/assets/file.js
+++ jsnack1/assets/file.js
@@ -4,7 +4,7 @@ let numeri = 0;
 while (numeri < 50) {
   let numero = parseInt(prompt("Inserisci un numero:"));
   array.push(numero);
-  sum += numero;
+  numeri += numero;
 }
 
 console.log("hai superato 50");
