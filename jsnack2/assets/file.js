202840ea19f2847fb6766a3e141b7ade6d0efe99
ndongogueye <bayeg2003@gmail.com>
2023-07-10 08:53:53 +0200

    eseecizio non corretto

---
+++ jsnack2/assets/file.js
@@ -0,0 +1,12 @@
+const array = [1,2,3,4,5,6,7,8,9,10];
+
+let rosso = document.getElementById('rosso');
+let verde = document.getElementById('verde');
+
+for (let i = 0; i < array.length; i++) {
+    if (array[i] % 3 == 0) {
+      rosso.innerHTML += array[i] + " ";
+    } else {
+      verde.innerHTML += array[i] + " ";
+    }
+  }
